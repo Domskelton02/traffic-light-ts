00e0d15735b84027ec340b88313760aec559b722
Dominick <adltech02@gmail.com>
2024-01-03 06:28:02 -0700

    second attempt at traffic-light assignment. refactored to typescript as recommended, attempted to simplify, and use the DRY method.

--- src/ClassTrafficLight.tsx
+++ src/ClassTrafficLight.tsx
@@ -1,17 +1,32 @@
-import { Component } from "react";
+import { Component } from 'react';
 
-export class ClassTrafficLight extends Component {
+type State = {
+  lights: string[];
+  lightIndex: number;
+};
+
+export class ClassTrafficLight extends Component<object, State> {
+  state: State = {
+    lights: ['red', 'yellow', 'green'],
+    lightIndex: 0
+  };
+
+  nextState = () => {
+    this.setState(prevState => ({ lightIndex: (prevState.lightIndex + 1) % prevState.lights.length }));
+  };
+
   render() {
+    const { lights, lightIndex } = this.state;
+
     return (
       <div className="traffic-light-box">
         <h2>Class Traffic Light</h2>
         <div className="traffic-light">
-          {/* Background color can be black | yellow | red | green */}
-          <div className="circle red"></div>
-          <div className="circle black"></div>
-          <div className="circle black"></div>
+          {lights.map((color, index) => (
+            <div key={color} className={`circle ${lightIndex === index ? color : 'black'}`}></div>
+          ))}
         </div>
-        <button className="next-state-button">Next State</button>
+        <button className="next-state-button" onClick={this.nextState}>Next State</button>
       </div>
     );
   }
--- src/FunctionalTrafficLight.tsx
+++ src/FunctionalTrafficLight.tsx
@@ -1,17 +1,22 @@
+import { useState } from 'react';
+
 export const FunctionalTrafficLight = () => {
+  const lights = ['red', 'yellow', 'green'];
+  const [lightIndex, setLightIndex] = useState(0);
+
+  const nextState = () => {
+    setLightIndex((lightIndex + 1) % lights.length);
+  };
+
   return (
     <div className="traffic-light-box">
       <h2>Functional Traffic Light</h2>
       <div className="traffic-light">
-        {/* Background color can be black | yellow | red | green */}
-        <div className="circle black"></div>
-        <div className="circle yellow"></div>
-        <div className="circle green"></div>
+        {lights.map((color, index) => (
+          <div key={color} className={`circle ${lightIndex === index ? color : 'black'}`}></div>
+        ))}
       </div>
-      <button className="next-state-button">Next State</button>
+      <button className="next-state-button" onClick={nextState}>Next State</button>
     </div>
   );
 };
-
-
-
